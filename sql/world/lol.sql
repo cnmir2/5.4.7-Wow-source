faccb035844718afe677f84464006541358bc58b
WoWSource <geany_100@yahoo.com>
2015-08-17 20:12:59 +0300

    Create lol.sql
    saying the truth

---
+++ sql/world/lol.sql
@@ -0,0 +1,7 @@
+people dont let your self fullsih oz is not so stupid and  this is a old core 
+he has 4 diffrent repos in case a shit like this will try to stolen and repost he has 4 diffrent repos  which  are 
+1 for fake 
+2 for free release
+3 for vip 
+4 for personal use only
+so the point is dont belive in shit and santa is dead  LOL
